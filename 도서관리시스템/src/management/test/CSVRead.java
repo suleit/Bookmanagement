927607a995780a47a451e3dc391b8806aa0cb5b8
suleit <39208843+suleit@users.noreply.github.com>
2018-05-14 15:11:13 +0900

    다시한번더 에러 많음;

--- 도서관리시스템/src/management/test/CSVRead.java
+++ 도서관리시스템/src/management/test/CSVRead.java
@@ -1,35 +1,38 @@
 package management.test;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
- 
-import au.com.bytecode.opencsv.CSVReader;
- 
+import com.opencsv.CSVReader;
+
 public class CSVRead {
- 
-    private String filename = "bookList.csv";
- 
-    public CSVRead() {}
- 
-    public List<String []> readCsv() {
- 
-        try {
-            CSVReader reader = new CSVReader(new FileReader(filename));
-            // UTF-8
-            // CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(filename), "UTF-8"), ",", '"', 1);
-            String[] s;
-            while ((s = reader.readNext()) != null) {
-                data.add(s);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
- 
-        return data;
+
+  private String filename = "C:\\Users\\suhyun\\git\\Bookmanagement\\도서관리시스템\\src\\bookList.csv";
+
+  public CSVRead() {}
+
+  public List<String[]> readCsv() {
+
+    List<String[]> data = new ArrayList<String[]>();
+
+    try {
+      // CSVReader reader = new CSVReader(new FileReader(filename), '\t');
+      // UTF-8
+      CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(filename), "MS949"));
+      
+      String[] s;
+
+      while ((s = reader.readNext()) != null) {
+        data.add(s);
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-}
+    return data;
+  } //
+}//class
--- 도서관리시스템/src/management/test/testDAo.java
+++ 도서관리시스템/src/management/test/testDAo.java
@@ -1,9 +1,35 @@
 package management.test;
 
+import java.util.ArrayList;
+
+import management.userDAO.BookDao;
+import management.vo.Book;
+
 public class testDAo {
 
 	public static void main(String[] args) {
+		
+		
+		CSVRead cr=new CSVRead();
+		BookDao bd=new BookDao();
+		ArrayList<String[]> excelRead=(ArrayList<String[]>) cr.readCsv();
+		ArrayList<Book> booklist=new ArrayList<>();
+		
+		//for (int i = 1; i < 2; i++) {
+				String[] strings= excelRead.get(1);
+				Book b=new Book(strings[0], strings[1], strings[2]);
+				System.out.println(b);
+				boolean result=bd.insertBook(b);
+				System.out.println(result);
+	//	}// for
+		
+		
+		
+		
 
-	}
-
+		
+	
+		
+		
+	}//main
 }
