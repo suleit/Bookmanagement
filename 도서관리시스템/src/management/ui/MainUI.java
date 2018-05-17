77b2de99183e6e80d5e607aa6cbf7d73904ffc5b
kkbs9876 <39208987+kkbs9876@users.noreply.github.com>
2018-05-17 09:36:25 +0900

    returnbutton

--- 도서관리시스템/src/management/ui/MainUI.java
+++ 도서관리시스템/src/management/ui/MainUI.java
@@ -384,7 +384,6 @@ public class MainUI extends JFrame implements ActionListener {
 	private BookDao bd = new BookDao();
 	private DefaultTableModel model, model2, modelP3, model_delete;
 	private JTable table_rental, table_return, p3_tables;
-
 	private JPanel panel_1, panel_2, panel_3, panel_4, panel_findbook;
 	private JLabel lblNewLabel;
 	private JTextField textField, findbook_tf, P1_TXTF;
@@ -573,12 +572,12 @@ public class MainUI extends JFrame implements ActionListener {
 							new String[] { "대여번호", "book.NO", "도서제목", "출판사", "대여자명", "대여일시", "예약자명", "연체일자" });
 					table_return.setModel(model2);
 				} else {
-					P2_RETURN.add(new JLabel("해당 이용자는 현재 대여중인 책이 없습니다."));
+					JOptionPane.showMessageDialog(this, "해당 이용자는 현재 대여중인 책이 없습니다.");
 				}
 				return_table_sc = new JScrollPane(table_return);
 				P2_RETURN.add(return_table_sc);
 			} else {
-				P2_RETURN.add(new JLabel("해당 이용자는 현재 대여중인 책이 없습니다."));
+				JOptionPane.showMessageDialog(this, "해당 이용자는 현재 대여중인 책이 없습니다.");
 			}
 		} // 반납조회 이벤트 종료
 		
